12691307e2c24c1a561a22f65a8e5ad50b33f80f
Thaslima Nilofer <nilofer.h94@gmail.com>
2026-02-27 01:44:31 +0400

    removed cors proxy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All";
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const RESTAURANT_LIST = `${CORS_PROXY}https://namastedev.com/api/v1/listRestaurants`;
-export const MENU_API = `${CORS_PROXY}https://namastedev.com/api/v1/listRestaurantMenu/`;
+export const RESTAURANT_LIST = `https://namastedev.com/api/v1/listRestaurants`;
+export const MENU_API = `https://namastedev.com/api/v1/listRestaurantMenu/`;
 export const DISH_IMAGE_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
